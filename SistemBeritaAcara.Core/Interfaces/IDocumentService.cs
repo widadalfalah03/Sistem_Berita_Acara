ec1d758b96ba99368ff5c5ba64c46023a3734547
Lif <luthfialifp@gmail.com>
2026-06-23 13:13:17 +0700

    fix bug hasil editor tdk muncul di docx yg di-download + fix notif

--- SistemBeritaAcara.Core/Interfaces/IDocumentService.cs
+++ SistemBeritaAcara.Core/Interfaces/IDocumentService.cs
@@ -5,6 +5,7 @@ namespace SistemBeritaAcara.Core.Interfaces;
 public interface IDocumentService
 {
     Task<string> GenerateDocxAsync(BeritaAcara ba);
+    Task PatchNomorSuratAsync(int baId, string nomorSurat);
     Task<string> EmbedTtdMenyerahkanAsync(int baId, string ttdPath);
     Task<string> EmbedTtdPreviewApproverAsync(int baId, string ttdPath);
     Task<string> EmbedTtdPjAsync(int baId, string ttdPath);
