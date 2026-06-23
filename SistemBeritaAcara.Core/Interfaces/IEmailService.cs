61922acb3ea062243958dd5428f5b3cfd2360711
widadalfalah03 <widadalfalah03@gmail.com>
2026-06-23 15:08:16 +0700

    Add H-1 notifikasi peminjaman

--- SistemBeritaAcara.Core/Interfaces/IEmailService.cs
+++ SistemBeritaAcara.Core/Interfaces/IEmailService.cs
@@ -7,7 +7,7 @@ public interface IEmailService
     Task SendTtdUsedNotificationAsync(string toEmail, string pegawaiNama, string nomorSurat);
     Task SendApprovalRequestAsync(string toEmail, string approverName, string baseUrl, int baId);
     Task SendApprovalResultAsync(string toEmail, string recipientName, string nomorSurat, bool approved, string? alasan = null);
-    Task SendDueDateReminderAsync(string toEmail, string recipientName, string nomorSurat, string pjNama, string tanggalKembali, bool isOverdue, bool isForPj);
+    Task SendDueDateReminderAsync(string toEmail, string recipientName, string nomorSurat, string pjNama, string tanggalKembali, int daysUntilDue, bool isForPj);
     Task SendUserInvitationAsync(string toEmail, string userName, string token, string baseUrl);
     Task SendPasswordResetLinkAsync(string toEmail, string userName, string token, string baseUrl);
 }
