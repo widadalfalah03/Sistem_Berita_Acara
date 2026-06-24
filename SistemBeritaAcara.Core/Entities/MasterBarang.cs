ea4f79b98a1b76a6d52fb8b05516a76079868eb3
widadalfalah03 <widadalfalah03@gmail.com>
2026-06-24 09:04:45 +0700

    clear db +hapus kode barang

--- SistemBeritaAcara.Core/Entities/MasterBarang.cs
+++ SistemBeritaAcara.Core/Entities/MasterBarang.cs
@@ -3,7 +3,7 @@ namespace SistemBeritaAcara.Core.Entities;
 public class MasterBarang
 {
     public int Id { get; set; }
-    public string KodeBarang { get; set; } = string.Empty;
+
     public string NamaBarang { get; set; } = string.Empty;
     public bool IsAktif { get; set; } = true;
 
