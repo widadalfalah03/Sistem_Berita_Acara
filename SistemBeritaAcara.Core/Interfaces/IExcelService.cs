d77f0aae73b1e74777ed267170e38359e22e336c
Romi Aprilian Mustafa <romiaprilian7406@gmail.com>
2026-06-30 11:06:54 +0700

    feat: CRUD pegawai dan barang

--- SistemBeritaAcara.Core/Interfaces/IExcelService.cs
+++ SistemBeritaAcara.Core/Interfaces/IExcelService.cs
@@ -8,4 +8,6 @@ public interface IExcelService
     Task<(int added, int updated, int deactivated, List<string> errors)> ImportBarangAsync(Stream excelStream, int importedBy, string fileName);
     Task AppendBeritaAcaraToArsipAsync(BeritaAcara ba);
     Task<byte[]> ExportArsipAsync(IEnumerable<BeritaAcara> data, string baseUrl);
+    Task<byte[]> ExportPegawaiAsync(IEnumerable<Pegawai> data);
+    Task<byte[]> ExportBarangAsync(IEnumerable<MasterBarang> data);
 }
