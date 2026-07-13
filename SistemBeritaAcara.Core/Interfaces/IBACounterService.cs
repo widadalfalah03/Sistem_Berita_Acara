c5147579fec098244bd8969c82f8e08fa23ac27f
Lif <luthfialifp@gmail.com>
2026-07-13 09:55:43 +0700

    fix logic numbering BA

--- SistemBeritaAcara.Core/Interfaces/IBACounterService.cs
+++ SistemBeritaAcara.Core/Interfaces/IBACounterService.cs
@@ -3,4 +3,5 @@ namespace SistemBeritaAcara.Core.Interfaces;
 public interface IBACounterService
 {
     Task<(int counterValue, string nomorSurat)> GetNextNomorSuratAsync(DateOnly tanggal, string jenis);
+    Task AdjustCounterAfterDeleteAsync();
 }
